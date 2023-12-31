f8bf157f242f184630b12b58829c92885222461b
Goreeeo <endmyaddress@gmail.com>
2023-12-31 01:29:21 +0100

    Initial Commit

---
+++ bin/nocturne_design.dart
@@ -0,0 +1,5 @@
+import 'package:nocturne_design/nocturne_design.dart' as nocturne_design;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nocturne_design.calculate()}!');
+}
---
+++ test/nocturne_design_test.dart
@@ -0,0 +1,6 @@
+import 'package:nocturne_design/nocturne_design.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+}
